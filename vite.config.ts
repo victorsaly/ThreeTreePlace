b632766278e3dd87256450665e8e7bff8a3fb573
victorsaly <info@victorsaly.com>
2025-02-20 11:53:41 +0100

    chore: update the base folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss(),
-  ],
+  plugins: [vue(), tailwindcss()],
   build: {
-    outDir: 'docs', // Specify the output directory
+    outDir: "docs", // Specify the output directory
   },
-  base: process.env.NODE_ENV === 'production'
-    ? '/ThreeTreePlace/' // Replace 'threetreeplace' with your repository name
-    : '/',
-})
+  base: "/ThreeTreePlace/",
+});
